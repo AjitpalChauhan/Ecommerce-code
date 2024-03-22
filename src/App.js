b01637bc965c6e84a8a80fe80473c91156808b8f
Ajit <ajit@ajit.com>
2024-03-22 19:42:38 +0530

    Commit2: create auth pages and setup Routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,40 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import ProductList from './features/product-list/ProductList';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage/>,
+  },
+  {
+    path: "/cart",
+    // element: <LoginPage/>,
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <RouterProvider router={router} />
     </div>
   );
 }
